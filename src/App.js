aca7bc0d3c895546da533f1f87c41e0cd7797f18
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2023-04-30 13:43:09 +0530

    Revert "error boundaries,life cycle methods"
    This reverts commit da3db2a9396a670a1e5f3b17f76473ca9d1d8bcf.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,25 @@
-import "./App.css";
-import React, { useState } from "react";
-import Counter from "./Counter";
+import logo from './logo.svg';
+import './App.css';
 
-// function App() {
-//   const [open, setOpen] = useState(false);
-//   const [count, setCounter] = useState(0);
-//   const handleCount = () => {
-//     setCounter(count + 1);
-//   };
-//   const showCounter  =()=>{
-//     setOpen(~open);
-//   }
-//   return (
-//     <div className="App">
-//       <button onClick={showCounter}>Toggle Counter</button>
-//       {open ? <Counter count={count} countSetter={handleCount} /> :""}
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-      open: false,
-    };
-  }
-  //!Note Error Boundaries example
-  componentDidUpdate(){
-    if(this.state.count < 0){
-        throw new Error('Negative Values are not allowed right');
-    }
-  }
-  render() {
-    const handleCount = (type) => {
-      if(type == 1)this.setState({count:this.state.count+1});
-      else this.setState({count:this.state.count-1});
-    };
-    
-    const showCounter = () => {
-      this.setState({open:~this.state.open});
-    };
-    return (
-      <>
-        <button onClick={showCounter}>Toggle Counter</button>
-        {this.state.open ? (
-          <Counter count={this.state.count} />
-        ) : (
-          ""
-        )}
-      </>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
